547bda1a2a8f51e2a7d69d1977a9db959bec8e35
lisiling <sli116@hawk.iit.edu>
2015-11-09 05:19:35 -0600

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,47 @@
+<html>
+<head><title>Gallery</title>
+</head>
+<body>
+
+<?php
+session_start();
+$email = $_POST["email"];
+echo $email;
+require 'vendor/autoload.php';
+
+use Aws\Rds\RdsClient;
+$client = RdsClient::factory(array(
+'version'=>'latest'
+'region'  => 'us-east-1'
+));
+
+$result = $client->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'lsl-db',
+));
+
+
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+    echo "============\n". $endpoint . "================";
+
+
+
+$link = mysqli_connect($endpoint,"lisiling","ilovebunnies","itmo544mp1") or die("Error " . mysqli_error($link));
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$link->real_query("SELECT * FROM mp1tb WHERE email = '$email'");
+
+$res = $link->use_result();
+echo "Result set order...\n";
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+echo $row['id'] . "Email: " . $row['email'];
+}
+$link->close();
+?>
+</body>
+</html>
