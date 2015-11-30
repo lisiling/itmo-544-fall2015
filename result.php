c28dda6eccbca1e2d77241f31cb71b775e275807
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-29 21:58:34 -0600

    add some css

--- result.php
+++ result.php
@@ -1,3 +1,51 @@
+<html>
+<head>
+
+<style type="text/css">
+body{
+        position: relative;
+        text-align: center;
+        opacity: 0.8;
+        width:1001px;
+        height:100%;
+        padding: 0px;
+        margin: 0px;
+        background: lightyellow;
+}
+header{
+        background: blue;
+        font-size: 30px;
+        text-align: center;
+        color: lightyellow;
+        text-shadow: 0 0 8px black;
+        opacity: 0.8;
+        width: 100%;
+        height: 140px;
+        margin-right: 0px;
+}
+#content{
+    font-size: 20px;
+    margin-top: 40px;
+    margin-left: 20px;
+    text-align: center;
+    color: blue;
+    text-shadow: 0 0 6px lightblue;
+}
+</style>
+
+<title>index</title>
+</head>
+
+<body>
+
+<header>
+<br/>
+	<p>Welcome to Siling's Page</p>
+</header>
+
+<div id="content">
+
+<br/><br/>
 <?php
 
 session_start();
@@ -94,3 +142,8 @@ echo $row['id'] . " " . $row['email']. " " . $row['phone'];
 $link->close();
 
 ?>
+</div>
+
+
+</body>
+</html>
