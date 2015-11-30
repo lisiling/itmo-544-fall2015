dceab32b183cffe8a92d5a289fda8b881373ede7
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-29 22:21:51 -0600

    change some php code

--- gallery.php
+++ gallery.php
@@ -49,8 +49,8 @@ header{
 
 <?php
 session_start();
+
 $email = $_POST["email"];
-echo $email;
 require 'vendor/autoload.php';
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
@@ -60,21 +60,20 @@ $client = RdsClient::factory(array(
 $result = $client->describeDBInstances([
    'DBInstanceIdentifier' => 'lsl-db',
 ]);
-
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
     echo "============\n". $endpoint . "================";
-
 $link = mysqli_connect($endpoint,"lisiling","ilovebunnies","itmo544mp1") or die("Error " . mysqli_error($link));
-/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+echo "\n" . $email . "gallery\n";
 
-$link->real_query("SELECT * FROM mp1tb WHERE email = '$email'");
+
+$link->real_query("SELECT * FROM items WHERE email = '$email'");
 
 $res = $link->use_result();
-echo "Result set order...\n";
+echo "Result...\n";
 while ($row = $res->fetch_assoc()) {
     echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
 echo $row['id'] . "Email: " . $row['email'];
